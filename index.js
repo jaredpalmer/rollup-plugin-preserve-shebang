526b75f3d145b35a2cebfd936e22e0964e7049bf
Jared Palmer <jared@palmer.net>
2019-04-29 14:52:06 -0400

    Rollup 1 compat

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import MagicString from 'magic-string';
 import fs from 'fs';
 
-export default function shebangPlugin({ shebang, entry }={}) {
+export default function shebangPlugin({ shebang, entry } = {}) {
 	function processEntry(entry) {
 		if (entry) {
 			let contents = fs.readFileSync(entry, 'utf8');
 			let matches = contents.match(/^\s*(#!.*)/);
-			shebang = matches && matches[1] || false;
+			shebang = (matches && matches[1]) || false;
 		}
 	}
 	processEntry(entry);
@@ -20,15 +20,15 @@ export default function shebangPlugin({ shebang, entry }={}) {
 			}
 			return options;
 		},
-		transformBundle(code, { format, sourcemap }) {
+		renderChunk(code, { format, sourcemap }) {
 			if (!shebang) return;
 
 			let str = new MagicString(code);
-			str.prepend(shebang+'\n');
+			str.prepend(shebang + '\n');
 			return {
 				code: str.toString(),
-				map: sourcemap ? str.generateMap({ hires: true }) : undefined
+				map: sourcemap ? str.generateMap({ hires: true }) : undefined,
 			};
-		}
+		},
 	};
 }
